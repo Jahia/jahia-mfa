87b27eb3710daf1602cc98b34c54e4d81a186a6d
Florent BOURASSE <fbourasse@jahia.com>
2020-09-28 14:17:09 +0200

    Initial commit

---
+++ src/main/resources/javascript/apps/registerExtensions.js
@@ -0,0 +1,34 @@
+window.jahia.i18n.loadNamespaces('jahia-2fa');
+
+window.jahia.uiExtender.registry.add('callback', 'jahia-2faExample', {
+    targets: ['jahiaApp-init:60'],
+    callback: function () {
+        window.jahia.uiExtender.registry.add('adminRoute', 'jahia-2faExample', {
+            targets: ['administration-sites:999', 'jahia-2faaccordion'],
+            label: 'jahia-2fa:label.settings.title',
+            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            isSelectable: true,
+            requireModuleInstalledOnSite: 'jahia-2fa',
+            iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.jahia-2fa.html.ajax'
+        });
+
+        window.jahia.uiExtender.registry.add('action', 'jahia-2faExample', {
+            buttonIcon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            buttonLabel: 'jahia-2fa:label.action.title',
+            targets: ['contentActions:999'],
+            onClick: context => {
+                window.open('https://github.com/Jahia/app-shell/blob/master/docs/declare-new-module.md', "_blank");
+            }
+        });
+
+        window.jahia.uiExtender.registry.add('accordionItem', 'jahia-2faApps_Example', window.jahia.uiExtender.registry.get('accordionItem', 'renderDefaultApps'), {
+            targets: ['jcontent:998'],
+            label: 'jahia-2fa:label.appsAccordion.title',
+            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            appsTarget: 'jahia-2faaccordion',
+            isEnabled: function(siteKey) {
+                return siteKey !== 'systemsite'
+            }
+        });
+    }
+});
