e1227e425fb96bd0dde05c50db920859886cae28
Fabrice Aissah <faissah@jahia.com>
2020-10-02 14:18:54 -0400

    Restructure MPA repo

---
+++ jahia-mfa-otp-provider/src/main/resources/javascript/apps/registerExtensions.js
@@ -0,0 +1,34 @@
+window.jahia.i18n.loadNamespaces('jahia-mfa-otp-provider');
+
+window.jahia.uiExtender.registry.add('callback', 'jahia-mfa-otp-providerExample', {
+    targets: ['jahiaApp-init:60'],
+    callback: function () {
+        window.jahia.uiExtender.registry.add('adminRoute', 'jahia-mfa-otp-providerExample', {
+            targets: ['administration-sites:999', 'jahia-mfa-otp-provideraccordion'],
+            label: 'jahia-mfa-otp-provider:label.settings.title',
+            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            isSelectable: true,
+            requireModuleInstalledOnSite: 'jahia-mfa-otp-provider',
+            iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.jahia-mfa-otp-provider.html.ajax'
+        });
+
+        window.jahia.uiExtender.registry.add('action', 'jahia-mfa-otp-providerExample', {
+            buttonIcon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            buttonLabel: 'jahia-mfa-otp-provider:label.action.title',
+            targets: ['contentActions:999'],
+            onClick: context => {
+                window.open('https://github.com/Jahia/app-shell/blob/master/docs/declare-new-module.md', "_blank");
+            }
+        });
+
+        window.jahia.uiExtender.registry.add('accordionItem', 'jahia-mfa-otp-providerApps_Example', window.jahia.uiExtender.registry.get('accordionItem', 'renderDefaultApps'), {
+            targets: ['jcontent:998'],
+            label: 'jahia-mfa-otp-provider:label.appsAccordion.title',
+            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            appsTarget: 'jahia-mfa-otp-provideraccordion',
+            isEnabled: function(siteKey) {
+                return siteKey !== 'systemsite'
+            }
+        });
+    }
+});
