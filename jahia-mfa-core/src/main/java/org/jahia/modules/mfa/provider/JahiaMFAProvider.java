12b8f0230bb79523de63ae6f75910387bc97a429
Florent BOURASSE <fbourasse@jahia.com>
2020-10-05 21:20:38 +0200

    Addition of methods for the registration process of the provider

--- jahia-mfa-core/src/main/java/org/jahia/modules/mfa/provider/JahiaMFAProvider.java
+++ jahia-mfa-core/src/main/java/org/jahia/modules/mfa/provider/JahiaMFAProvider.java
@@ -1,10 +1,12 @@
 package org.jahia.modules.mfa.provider;
 
+import org.jahia.modules.mfa.service.JahiaMFAService;
 import org.jahia.services.content.decorator.JCRUserNode;
 
 public abstract class JahiaMFAProvider {
 
     private final String key;
+    private JahiaMFAService jahiaMFAService;
 
     public JahiaMFAProvider(String key) {
         this.key = key;
@@ -15,4 +17,21 @@ public abstract class JahiaMFAProvider {
     }
 
     public abstract boolean verifyToken(JCRUserNode userNode, String token);
+
+    public final void setJahiaMFAService(JahiaMFAService jahiaMFAService) {
+        this.jahiaMFAService = jahiaMFAService;
+    }
+
+    public final JahiaMFAService getJahiaMFAService() {
+        return jahiaMFAService;
+    }
+
+    public final void register() {
+        jahiaMFAService.addProvider(this);
+    }
+
+    public final void unregister() {
+        jahiaMFAService.addProvider(this);
+    }
+
 }
