cd7c8d6d445f09b0ea6ea77fa0985702642a520d
Florent BOURASSE <fbourasse@jahia.com>
2020-10-09 15:24:02 +0200

    AES encryption put into place for the secret key thanks to the user password

--- jahia-mfa-otp-provider/src/main/java/org/jahia/modules/mfa/otp/provider/JahiaMFAOtpProvider.java
+++ jahia-mfa-otp-provider/src/main/java/org/jahia/modules/mfa/otp/provider/JahiaMFAOtpProvider.java
@@ -1,6 +1,10 @@
 package org.jahia.modules.mfa.otp.provider;
 
+import java.security.Key;
 import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import javax.jcr.RepositoryException;
 import org.apache.commons.codec.binary.Base32;
 import org.jahia.modules.mfa.MFAConstants;
@@ -11,19 +15,21 @@ import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.content.JCRTemplate;
 import org.jahia.services.content.decorator.JCRUserNode;
 import org.jboss.aerogear.security.otp.Totp;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JahiaMFAOtpProvider extends JahiaMFAProvider {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JahiaMFAOtpProvider.class);
+    private static final String ALGORITHM = "AES";
     private static final String KEY = "jahia-mfa-otp-provider";
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(JahiaMFAOtpProvider.class);
 
     public JahiaMFAOtpProvider() {
         super(KEY);
     }
 
     @Override
-    public boolean verifyToken(JCRUserNode userNode, String token) {
+    public boolean verifyToken(JCRUserNode userNode, String token, String password) {
 
         try {
             return JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Boolean>() {
@@ -31,8 +37,8 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
                 public Boolean doInJCR(JCRSessionWrapper jcrsession) throws RepositoryException {
                     final JCRNodeWrapper defaultUserNode = jcrsession.getNode(userNode.getPath());
                     final JCRNodeWrapper mfaNode = defaultUserNode.getNode(MFAConstants.NODE_NAME_MFA);
-                    final String secretKey = mfaNode.getPropertyAsString(Constants.PROP_SECRET_KEY);
-                    final Totp totp = new Totp(secretKey);
+                    final String encryptedSecretKey = mfaNode.getPropertyAsString(Constants.PROP_SECRET_KEY);
+                    final Totp totp = new Totp(decryptSecretKey(encryptedSecretKey, password));
                     return isValidLong(token) && totp.verify(token);
                 }
             });
@@ -43,7 +49,7 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
     }
 
     @Override
-    public boolean activateMFA(JCRUserNode userNode) {
+    public boolean activateMFA(JCRUserNode userNode, String password) {
         try {
             return JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Boolean>() {
                 @Override
@@ -51,12 +57,15 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
                     final JCRNodeWrapper defaultUserNode = jcrsession.getNode(userNode.getPath());
                     final JCRNodeWrapper mfaNode = defaultUserNode.getNode(MFAConstants.NODE_NAME_MFA);
                     mfaNode.addMixin(Constants.MIXIN_MFA_OTP);
-                    mfaNode.setProperty(Constants.PROP_SECRET_KEY, generateSecretKey());
+                    mfaNode.setProperty(Constants.PROP_SECRET_KEY, encryptSecretKey(generateSecretKey(), password));
                     jcrsession.save();
                     return true;
                 }
             });
         } catch (RepositoryException ex) {
+            LOGGER.error(String.format("Impossible to activate MFA OTP for user %s", userNode.getUserKey()), ex);
+            return false;
+        } catch (IllegalStateException ex) {
             LOGGER.error(String.format("Impossible to activate MFA OTP for user %s", userNode.getUserKey()), ex);
             return false;
         }
@@ -84,6 +93,39 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
             LOGGER.error(String.format("Impossible to deactivate MFA OTP for user %s", userNode.getUserKey()), ex);
             return false;
         }
+    }
+
+    private static String encryptSecretKey(String secretKey, String password) {
+        try {
+            final Key key = generateKey(password);
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            final byte[] encValue = cipher.doFinal(secretKey.getBytes());
+            final String encryptedSecretKey = Base64.getEncoder().encodeToString(encValue);
+            return encryptedSecretKey;
+        } catch (Exception ex) {
+            throw new IllegalStateException("Impossible to encrypt secret key", ex);
+        }
+    }
+
+    public static String decryptSecretKey(String encryptedSecretKey, String password) {
+        try {
+
+            final Key key = generateKey(password);
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            final byte[] decordedValue = Base64.getDecoder().decode(encryptedSecretKey);
+            final byte[] decValue = cipher.doFinal(decordedValue);
+            final String secretKey = new String(decValue);
+            return secretKey;
+        } catch (Exception ex) {
+            throw new IllegalStateException("Impossible to decrypt secret key", ex);
+        }
+    }
+
+    private static Key generateKey(String password) {
+        final Key key = new SecretKeySpec(password.getBytes(), ALGORITHM);
+        return key;
     }
 
     private static String generateSecretKey() {
