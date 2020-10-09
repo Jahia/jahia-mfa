9e4e0c8ccd25b57bed70bedd7960b9b69f8a58ce
Florent BOURASSE <fbourasse@jahia.com>
2020-10-09 14:19:07 +0200

    Addition of constants

---
+++ jahia-mfa-otp-provider/src/main/java/org/jahia/modules/mfa/otp/provider/Constants.java
@@ -0,0 +1,11 @@
+package org.jahia.modules.mfa.otp.provider;
+
+public final class Constants {
+
+    public static final String MIXIN_MFA_OTP = "jmix:MFAOTP";
+    public static final String PROP_SECRET_KEY = "secretKey";
+
+    private Constants() {
+    }
+
+}
