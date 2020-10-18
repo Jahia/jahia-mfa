c3ba8573202200e4eca06ac9b7c9cccba425b471
Florent BOURASSE <fbourasse@jahia.com>
2020-10-18 10:48:36 +0200

    Password length adapted to expected encryption key size

--- jahia-mfa-otp-provider/src/main/java/org/jahia/modules/mfa/otp/provider/JahiaMFAOtpProvider.java
+++ jahia-mfa-otp-provider/src/main/java/org/jahia/modules/mfa/otp/provider/JahiaMFAOtpProvider.java
@@ -24,7 +24,8 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
     private static final Logger LOGGER = LoggerFactory.getLogger(JahiaMFAOtpProvider.class);
     private static final String ALGORITHM = "AES";
     private static final String KEY = "jahia-mfa-otp-provider";
-    private static final int SECRET_KEY_BYTE_SIZE = 20;
+    private static final int KEY_SIZE = 32;
+    private static final int TOTP_KEY_BYTE_SIZE = 20;
 
     public JahiaMFAOtpProvider() {
         super(KEY);
@@ -40,7 +41,7 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
                     final JCRNodeWrapper defaultUserNode = jcrsession.getNode(userNode.getPath());
                     final JCRNodeWrapper mfaNode = defaultUserNode.getNode(MFAConstants.NODE_NAME_MFA);
                     final String encryptedSecretKey = mfaNode.getPropertyAsString(Constants.PROP_SECRET_KEY);
-                    final Totp totp = new Totp(decryptSecretKey(encryptedSecretKey, password));
+                    final Totp totp = new Totp(decryptTotpSecretKey(encryptedSecretKey, password));
                     return isValidLong(token) && totp.verify(token);
                 }
             });
@@ -59,7 +60,7 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
                     final JCRNodeWrapper defaultUserNode = jcrsession.getNode(userNode.getPath());
                     final JCRNodeWrapper mfaNode = defaultUserNode.getNode(MFAConstants.NODE_NAME_MFA);
                     mfaNode.addMixin(Constants.MIXIN_MFA_OTP);
-                    mfaNode.setProperty(Constants.PROP_SECRET_KEY, encryptSecretKey(generateSecretKey(), password));
+                    mfaNode.setProperty(Constants.PROP_SECRET_KEY, encryptTotpSecretKey(generateTotpSecret(), password));
                     jcrsession.save();
                     return true;
                 }
@@ -94,9 +95,9 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
         }
     }
 
-    private static String encryptSecretKey(String secretKey, String password) {
+    private static String encryptTotpSecretKey(String secretKey, String password) {
         try {
-            final Key key = generateKey(password);
+            final Key key = generateSecretKey(password);
             final Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
             final byte[] encValue = cipher.doFinal(secretKey.getBytes(Charsets.UTF_8));
@@ -106,10 +107,10 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
         }
     }
 
-    public static String decryptSecretKey(String encryptedSecretKey, String password) {
+    public static String decryptTotpSecretKey(String encryptedSecretKey, String password) {
         try {
 
-            final Key key = generateKey(password);
+            final Key key = generateSecretKey(password);
             final Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, key);
             final byte[] decValue = cipher.doFinal(Base64.getDecoder().decode(encryptedSecretKey));
@@ -119,13 +120,22 @@ public class JahiaMFAOtpProvider extends JahiaMFAProvider {
         }
     }
 
-    private static Key generateKey(String password) {
-        return new SecretKeySpec(password.getBytes(Charsets.UTF_8), ALGORITHM);
+    private static Key generateSecretKey(String password) {
+        return new SecretKeySpec(generateKey(password).getBytes(Charsets.UTF_8), ALGORITHM);
+    }
+    
+    private static String generateKey(String password) {
+        String aesKey = "";
+        while (aesKey.length() < KEY_SIZE) {
+            aesKey = aesKey + password;
+        }
+        aesKey = aesKey.substring(0, KEY_SIZE);
+        return aesKey;
     }
 
-    private static String generateSecretKey() {
+    private static String generateTotpSecret() {
         final SecureRandom random = new SecureRandom();
-        byte[] bytes = new byte[SECRET_KEY_BYTE_SIZE];
+        byte[] bytes = new byte[TOTP_KEY_BYTE_SIZE];
         random.nextBytes(bytes);
         final Base32 base32 = new Base32();
         return base32.encodeToString(bytes);
