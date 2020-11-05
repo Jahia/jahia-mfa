514cd5607979073f0881c6e469c4fda52bc95c2b
Florent BOURASSE <fbourasse@jahia.com>
2020-11-05 18:54:56 +0100

    Action to verify a token

--- jahia-mfa-core/src/main/java/org/jahia/modules/mfa/MFAConstants.java
+++ jahia-mfa-core/src/main/java/org/jahia/modules/mfa/MFAConstants.java
@@ -8,6 +8,7 @@ public final class MFAConstants {
     public static final String NODE_TYPE_MFA = "jnt:MFA";
     public static final String PARAM_PASSWORD = "password";
     public static final String PARAM_PROVIDER = "provider";
+    public static final String PARAM_TOKEN = "token";
     public static final String PROP_ACTIVATED = "activated";
     public static final String PROP_PROVIDER = "provider";
     public static final int TOKEN_SIZE = 6;
--- jahia-mfa-core/src/main/java/org/jahia/modules/mfa/actions/VerifyTokenAction.java
+++ jahia-mfa-core/src/main/java/org/jahia/modules/mfa/actions/VerifyTokenAction.java
@@ -3,19 +3,66 @@ package org.jahia.modules.mfa.actions;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jahia.api.Constants;
 import org.jahia.bin.Action;
 import org.jahia.bin.ActionResult;
+import org.jahia.modules.mfa.MFAConstants;
+import org.jahia.modules.mfa.impl.JahiaMFAServiceImpl;
 import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.decorator.JCRUserNode;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.URLResolver;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VerifyTokenAction extends Action {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VerifyTokenAction.class);
 
     @Override
     public ActionResult doExecute(HttpServletRequest req, RenderContext renderContext, Resource resource,
             JCRSessionWrapper session, Map<String, List<String>> parameters, URLResolver urlResolver) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final JCRUserNode userNode = session.getUserNode();
+        try {
+
+            String password = null;
+            if (parameters.containsKey(MFAConstants.PARAM_PASSWORD)) {
+                final List<String> passwordValues = parameters.get(MFAConstants.PARAM_PASSWORD);
+                if (!passwordValues.isEmpty()) {
+                    password = passwordValues.get(0);
+                }
+            }
+
+            String provider = null;
+            if (parameters.containsKey(MFAConstants.PARAM_PROVIDER)) {
+                final List<String> providerValues = parameters.get(MFAConstants.PARAM_PROVIDER);
+                if (!providerValues.isEmpty()) {
+                    provider = providerValues.get(0);
+                }
+            }
+
+            String token = null;
+            if (parameters.containsKey(MFAConstants.PARAM_TOKEN)) {
+                final List<String> tokenValues = parameters.get(MFAConstants.PARAM_TOKEN);
+                if (!tokenValues.isEmpty()) {
+                    token = tokenValues.get(0);
+                }
+            }
+
+            if (password != null && provider != null && token != null && userNode != null && !userNode.getJahiaUser().getUsername().equals(Constants.GUEST_USERNAME)) {
+                final JahiaMFAServiceImpl jahiaMFAServiceImpl = JahiaMFAServiceImpl.getInstance();
+                final boolean result = jahiaMFAServiceImpl.verifyToken(userNode, provider, token, password);
+                final ActionResult actionResult = new ActionResult(HttpServletResponse.SC_OK, null, new JSONObject().put("result", result));
+                return actionResult;
+            }
+        } catch (Exception ex) {
+            LOGGER.error(String.format("Impossible to verity token for user %s", userNode.getPath()), ex);
+            return ActionResult.BAD_REQUEST;
+        }
+        return ActionResult.BAD_REQUEST;
     }
 
 }
