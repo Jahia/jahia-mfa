1502a88da84a2d42e846c76b06fdba0e064a0203
Florent BOURASSE <fbourasse@jahia.com>
2020-12-14 14:56:54 +0100

    Addition of a missing import

--- jahia-mfa-otp-provider/src/main/java/org/jahia/modules/mfa/otp/provider/actions/DecryptOtpKeyAction.java
+++ jahia-mfa-otp-provider/src/main/java/org/jahia/modules/mfa/otp/provider/actions/DecryptOtpKeyAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.jahia.bin.Action;
 import org.jahia.bin.ActionResult;
+import org.jahia.modules.mfa.graphql.extensions.Utils;
 import org.jahia.modules.mfa.MFAConstants;
 import org.jahia.modules.mfa.otp.provider.JahiaMFAOtpProvider;
 import org.jahia.services.content.JCRNodeWrapper;
